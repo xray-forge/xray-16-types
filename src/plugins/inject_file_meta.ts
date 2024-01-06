322509999e9f35624eee6a5949c15cde78d98092
Neloreck <Neloreck@gmail.com>
2024-01-06 18:38:52 +0200

    Add $dirname identifier in plugin with meta.

--- src/plugins/inject_file_meta.ts
+++ src/plugins/inject_file_meta.ts
@@ -4,6 +4,7 @@ import { SyntaxKind } from "typescript";
 import { createStringLiteral, Plugin } from "typescript-to-lua";
 
 const FILENAME_IDENTIFIER: string = "$filename";
+const DIRNAME_IDENTIFIER: string = "$dirname";
 
 /**
  * Plugin that injects FILE_NAME in compile-time.
@@ -13,6 +14,10 @@ const plugin: Plugin = {
     [SyntaxKind.Identifier]: (node, context) => {
       if (node.text === FILENAME_IDENTIFIER) {
         return createStringLiteral(path.parse(context.sourceFile.fileName).name);
+      }
+
+      if (node.text === DIRNAME_IDENTIFIER) {
+        return createStringLiteral(path.basename(path.dirname(context.sourceFile.fileName)));
       }
 
       return context.superTransformExpression(node);
