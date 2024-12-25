9b220138a92c0910cec0ed2218447793948869e0
Neloreck <Neloreck@gmail.com>
2024-12-25 23:37:09 +0200

    FS parameter names / corrections.

--- types/xr_lib/xr_fs.d.ts
+++ types/xr_lib/xr_fs.d.ts
@@ -72,45 +72,41 @@ declare module "xray16" {
     public static FS_sort_by_size_down: 3;
     public static FS_sort_by_size_up: 2;
 
-    public append_path(a: string, b: string, c: string, d: number): unknown /* FS_Path */;
+    public dir_delete(path: string, filename: string, remove_files: boolean): void;
 
-    public dir_delete(fs: FS, a: string, b: string, c: number): void;
+    public dir_delete(path: string, remove_files: boolean): void;
 
-    public dir_delete(fs: FS, a: string, c: number): void;
+    public file_list_open(alias: string, folder: string, flags: u32): FS_file_list;
 
-    public exist(folderAlias: string, filename: string, fs_type: TXR_fs_type): i32 | null;
+    public file_list_open(path: string, flags: u32): FS_file_list;
 
-    public exist(folderAlias: string, filename: string): i32 | null;
+    public file_list_open_ex(path: string, flags: u32, mask: string): FS_file_list_ex;
+
+    public file_copy(source: string, destination: string): void;
+
+    public file_length(path: string): i32;
+
+    public file_rename(path: string, destination: string, overwrite: boolean): void;
+
+    public get_file_age(path: string): u32;
+
+    public get_file_age_str(path: string): u32;
+
+    public file_delete(path: string, filename: string): void;
+
+    public file_delete(path: string): void;
+
+    public exist(alias: string, filename: string, fs_type: TXR_fs_type): i32 | null;
+
+    public exist(alias: string, filename: string): i32 | null;
 
     public exist(path: string): i32 | null;
 
-    public file_copy(a: string, b: string): void;
+    public append_path(alias: string, root: string, path: string, recursive: boolean): unknown /* FS_Path */;
 
-    public file_length(a: string): i32;
+    public path_exist(path: string): boolean;
 
-    public file_list_open(a: string, b: string, c: u32): FS_file_list;
-
-    public file_list_open(a: string, b: u32): FS_file_list;
-
-    public file_list_open_ex(a: string, b: u32, c: string): FS_file_list_ex;
-
-    public file_rename(a: string, b: string, c: boolean): void;
-
-    public get_file_age(a: string): u32;
-
-    public get_file_age_str(fs: FS, a: string): u32;
-
-    public path_exist(a: string): boolean;
-
-    public r_close(reader: reader): void;
-
-    public r_open(a: string): reader;
-
-    public r_open(a: string, b: string): reader;
-
-    public update_path(alias: string, addition: string): string;
-
-    public w_close(writer: unknown /* IWriter */): void;
+    public update_path(alias: string, add: string): string;
 
     public rescan_path(path: string): void;
 
@@ -119,13 +115,17 @@ declare module "xray16" {
      */
     public get_path(alias: string): unknown;
 
-    public file_delete(a: string, b: string): void;
+    public r_close(reader: reader): void;
 
-    public file_delete(fs: string): void;
+    public r_open(path: string): reader;
 
-    public w_open(a: string, b: string): unknown /* IWriter */;
+    public r_open(alias: string, path: string): reader;
 
-    public w_open(a: string): unknown /* IWriter */;
+    public w_close(writer: unknown /* IWriter */): void;
+
+    public w_open(path: string, filename: string): unknown /* IWriter */;
+
+    public w_open(path: string): unknown /* IWriter */;
   }
 
   /**
