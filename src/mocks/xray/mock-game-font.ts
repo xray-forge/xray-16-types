fa9a5ee83d0aa6d7d8e6872a851e0911414ad50b
Neloreck <Neloreck@gmail.com>
2026-07-08 03:31:57 +0300

    Expand font mocks to implement `CGameFont` interface, centralize font getter mocks, and export game font constants.

--- src/mocks/xray/mock-game-font.ts
+++ src/mocks/xray/mock-game-font.ts
@@ -1,15 +1,34 @@
+import { jest } from "@jest/globals";
+import { type CGameFont } from "xray16";
+
 /**
  * Mock of the X-Ray engine `CGameFont` class for jest/node.
  */
-export class MockCGameFont {
+export class MockCGameFont implements CGameFont {
   public static readonly alCenter: number = 2;
   public static readonly alLeft: number = 0;
   public static readonly alRight: number = 1;
+
+  public static mock(): CGameFont {
+    return new this() as unknown as CGameFont;
+  }
+
+  public static create(): MockCGameFont {
+    return new this();
+  }
 }
 
 /**
- * Mock of the engine `GetFontLetterica16Russian` global.
+ * Mock of the engine font getter globals.
  */
-export const mockGetFontLetterica16Russian = jest.fn((): MockCGameFont => {
-  return new MockCGameFont();
-});
+export const mockGetGameFont = jest.fn((): CGameFont => MockCGameFont.mock());
+export const mockGetFontDI = mockGetGameFont;
+export const mockGetFontGraffiti19Russian = mockGetGameFont;
+export const mockGetFontGraffiti22Russian = mockGetGameFont;
+export const mockGetFontGraffiti32Russian = mockGetGameFont;
+export const mockGetFontGraffiti50Russian = mockGetGameFont;
+export const mockGetFontLetterica16Russian = mockGetGameFont;
+export const mockGetFontLetterica18Russian = mockGetGameFont;
+export const mockGetFontLetterica25 = mockGetGameFont;
+export const mockGetFontMedium = mockGetGameFont;
+export const mockGetFontSmall = mockGetGameFont;
--- src/mocks/xray16-runtime.ts
+++ src/mocks/xray16-runtime.ts
@@ -1,6 +1,18 @@
 import { jest } from "@jest/globals";
 
 import { MockDevice } from "./xray/mock-device";
+import {
+  mockGetFontDI,
+  mockGetFontGraffiti19Russian,
+  mockGetFontGraffiti22Russian,
+  mockGetFontGraffiti32Russian,
+  mockGetFontGraffiti50Russian,
+  mockGetFontLetterica16Russian,
+  mockGetFontLetterica18Russian,
+  mockGetFontLetterica25,
+  mockGetFontMedium,
+  mockGetFontSmall,
+} from "./xray/mock-game-font";
 import { MockCGameGraph } from "./xray/mock-game-graph";
 import { MockAlifeSimulator } from "./xray/server/mock-alife-simulator";
 
@@ -11,6 +23,7 @@ import { MockAlifeSimulator } from "./xray/server/mock-alife-simulator";
 export { MockVector as vector } from "./xray/mock-vector";
 export { MockVector2D as vector2 } from "./xray/mock-vector-2d";
 export { MockCTime as CTime } from "./xray/mock-ctime";
+export { MockCGameFont as CGameFont } from "./xray/mock-game-font";
 export { MockCSightParams as CSightParams } from "./xray/mock-sight-params";
 export { mockGameInterface as game } from "./xray/mock-game";
 export { mockLevelInterface as level } from "./xray/mock-level";
@@ -18,6 +31,16 @@ export { mockLevelInterface as level } from "./xray/mock-level";
 export const alife = jest.fn(() => MockAlifeSimulator.mock());
 export const device = jest.fn(() => MockDevice.getInstance());
 export const game_graph = jest.fn(() => MockCGameGraph.getInstance());
+export const GetFontDI = mockGetFontDI;
+export const GetFontGraffiti19Russian = mockGetFontGraffiti19Russian;
+export const GetFontGraffiti22Russian = mockGetFontGraffiti22Russian;
+export const GetFontGraffiti32Russian = mockGetFontGraffiti32Russian;
+export const GetFontGraffiti50Russian = mockGetFontGraffiti50Russian;
+export const GetFontLetterica16Russian = mockGetFontLetterica16Russian;
+export const GetFontLetterica18Russian = mockGetFontLetterica18Russian;
+export const GetFontLetterica25 = mockGetFontLetterica25;
+export const GetFontMedium = mockGetFontMedium;
+export const GetFontSmall = mockGetFontSmall;
 export const time_global = jest.fn((): number => 0);
 export const verify_if_thread_is_running = jest.fn((): void => {});
 export const log = jest.fn((): void => {});
